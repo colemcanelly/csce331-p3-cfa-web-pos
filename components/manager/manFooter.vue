98916d529cfb5801882fe03149fa7329023ce1ec
Ryan Paul <ryan27paul@gmail.com>
2023-04-18 00:22:20 -0500

    Redesigned manger layout

---
+++ components/manager/manFooter.vue
@@ -0,0 +1,46 @@
+<!-- CUSTOMER FOOTER -->
+<template>
+    <v-bottom-navigation app color="primary">
+      <v-btn v-for="(item, i) in items" nuxt :key="i" :to="item.to" :value="item.label">
+        <v-icon>{{ item.icon }}</v-icon>
+        <span>{{ item.label }}</span>
+      </v-btn>
+    </v-bottom-navigation>
+  </template>
+  
+  <!-- ICONS: https://pictogrammers.com/library/mdi/ -->
+  <script>
+  export default {
+    data: function () {
+      return {
+        items: [
+          {
+            icon: 'mdi-map-marker',
+            label: 'Menu List',
+            to: '/managerMenu'
+          },
+          {
+            icon: 'mdi-silverware-fork-knife',
+            label: 'Inventory',
+            to: '/managerInventory'
+          },
+          {
+            icon: 'mdi-shopping',
+            label: 'Sales Report',
+            to: '/salesReport'
+          },
+        //   {
+        //     icon: 'mdi-shopping',
+        //     label: 'XZ Report',
+        //     to: '/customer/Order'
+        //   },
+        //   {
+        //     icon: 'mdi-shopping',
+        //     label: 'Excess Report',
+        //     to: '/customer/Order'
+        //   },
+        ],
+      };
+    }
+  }
+  </script>
