b971c9b79dd40e6e11be5b7e594586a8df1795da
Logan Kettle <lgk24@me.com>
2023-04-26 12:30:46 -0500

    functioning Maps API

---
+++ pages/customer/Nearby.vue
@@ -0,0 +1,34 @@
+<template>
+<v-app>
+    <div id="map" style="height: 500px;"></div>
+</v-app>
+</template>
+  
+  <script>
+  export default {
+
+    name: "NearbyPage",
+    layout: 'customer',
+    mounted() {
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAe16C2FwOAOUeGi6L_YhFjlgYrxus-uQw&callback=initMap`;
+      script.defer = true;
+      script.async = true;
+  
+      window.initMap = () => {
+        const map = new google.maps.Map(document.getElementById('map'), {
+          center: { lat: 30.6219, lng: -96.3263 },
+          zoom: 12
+        });
+      };
+  
+      document.head.appendChild(script);
+    }
+  }
+  </script>
+  
+
+
+
+
+   
