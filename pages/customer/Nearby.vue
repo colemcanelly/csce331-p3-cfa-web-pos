503863a9f3d0d118ed58a48cee8bedac89b0ff87
Logan Kettle <lgk24@me.com>
2023-04-26 13:39:35 -0500

    trying to format

--- pages/customer/Nearby.vue
+++ pages/customer/Nearby.vue
@@ -1,6 +1,13 @@
 <template>
 <v-app>
-    <div id="map" style="height: 500px;"></div>
+    <v-row>
+      <v-col cols="12"><v-card ><v-card-title class="text-center">Nearby Chick-Fil-A Locations</v-card-title ></v-card></v-col>
+      
+        <v-col cols="12"><v-card id="map" style="height: 500px;"></v-card></v-col>
+    
+      </v-row>
+
+    
 </v-app>
 </template>
   
@@ -9,6 +16,17 @@
 
     name: "NearbyPage",
     layout: 'customer',
+    data() {
+      return {
+        coordinates: [
+          { lat: 30.61714, lng: -96.34383 },
+          { lat: 30.61246, lng: -96.34091 },
+          { lat: 30.61622, lng: -96.31972 },
+          { lat: 30.63101, lng: -96.37925 },
+          { lat: 30.65970, lng: -96.33103 },
+        ],
+      };
+    },
     mounted() {
       const script = document.createElement('script');
       script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAe16C2FwOAOUeGi6L_YhFjlgYrxus-uQw&callback=initMap`;
@@ -19,6 +37,13 @@
         const map = new google.maps.Map(document.getElementById('map'), {
           center: { lat: 30.6219, lng: -96.3263 },
           zoom: 12
+        });
+
+        this.coordinates.forEach((coordinate) => {
+          new google.maps.Marker({
+            position: coordinate,
+            map,
+          });
         });
       };
   
