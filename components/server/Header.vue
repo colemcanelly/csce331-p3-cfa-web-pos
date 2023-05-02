712a9921cd95d1a58816d9a04e6403d406f36643
Weston Cadena <westoncadena@gmail.com>
2023-05-02 13:40:38 -0500

    Created Excess Report

--- components/server/Header.vue
+++ components/server/Header.vue
@@ -8,14 +8,25 @@
     <v-spacer></v-spacer>
     <!-- <span>Total Price: {{ totalPrice }}</span> -->
     <div id="google_translate_element"></div>
+    <style>
+    #google_translate_element select.goog-te-combo {
+      border: 1px solid #808080;
+      border-radius: 4px;
+      padding: 4px;
+    }
+    body > .skiptranslate {
+    display: none;
+    } 
+    </style>
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+          new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+        }
+    </script>
+    <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-          <script type="text/javascript">
-          function googleTranslateElementInit() {
-            new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
-          }
-          </script>
 
-          <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+
     <v-spacer></v-spacer>
     <h2 class="blue-grey--text text--darken-3">{{ kiosk_num }}</h2>
   </v-app-bar>
--- pages/report/excessReport.vue
+++ pages/report/excessReport.vue
@@ -8,7 +8,7 @@
   </template>
   
   <script>
-  import Report from '~/components/report/Report.vue'
+  import Report from '~/components/report/ExcessReport.vue'
   export default {
   name: 'excessReportPage',
   layout: 'report',
@@ -20,8 +20,9 @@
     table: 'excess-report',
     headers: [
       { text: 'Ingredient', value: 'ingredient', sortable: false },
-      { text: 'Threshold', value: 'threshold' },
-      { text: 'Restock Quantity', value: 'restock_quantity' },
+      { text: 'Start Quantity', value: 'qty_sod' },
+      { text: 'Quatity Sold', value: 'total_qty_sold' },
+      { text: 'Percent Difference', value: 'percentage_diff' },
     ],
   })
   }
