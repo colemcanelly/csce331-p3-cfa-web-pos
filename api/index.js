3c2dfa39be2fbcd4d716785f1abc39f30271d3a0
Ryan Paul <ryan27paul@gmail.com>
2023-05-02 18:21:54 -0500

    updated some api calls for manager

--- api/index.js
+++ api/index.js
@@ -196,6 +196,72 @@ app.get("/recipes", async (req, res) => {
     }
 });
 
+//Get all ingredients
+app.post("/ingredients", async (req, res) => {
+    try {
+        const allTodos = await pool.query("SELECT ingredient,portion_count FROM recipes");
+        res.json(allTodos.rows)
+    }
+    catch (err) {
+        console.error(err.message);
+    }
+});
+
+app.post("/itemIngredients", async (req, res) => {
+    try {
+        const { menu_item } = req.body;
+        const allTodos = await pool.query("SELECT ingredient,portion_count FROM recipes WHERE menu_item = '" + menu_item + "';");
+        res.json(allTodos.rows)
+    }
+    catch (err) {
+        console.error(err.message);
+    }
+});
+//updates an menu item's recipe
+app.put("/itemRecipe", async (req, res) => {
+    try {
+      const { menu_item, ingredient, portion_count } = req.body;
+      await pool.query(
+        "UPDATE recipes SET portion_count = $1 WHERE menu_item = $2 AND ingredient = $3",
+        [portion_count, menu_item, ingredient]
+      );
+      res.send("Recipe updated successfully");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
+//inserts a new ingredient 
+  app.post("/itemRecipe", async (req, res) => {
+    try {
+        console.log("here");
+        const { menu_item, ingredient, portion_count } = req.body;
+        const q = `INSERT INTO recipes (menu_item, ingredient, portion_count)
+      VALUES ('${menu_item}', '${ingredient}', ${portion_count});`;
+        console.log("This: ",q);
+        const newItem = await pool.query(q);
+        res.json(newItem.rows[0]);
+    }
+    catch (err) {
+        console.error('Error posting new menu item:', err);
+        res.status(500).send('Error posting new menu item');
+    }
+});
+  
+app.delete("/itemRecipe", async (req, res) => {
+    try {
+        console.log("reached delete");
+        const { menu_item, ingredient} = req.body;
+        const delete_recipe_item = await pool.query(`
+        DELETE FROM recipes 
+        WHERE menu_item = $1 AND ingredient = $2;`, [menu_item, ingredient]);
+        res.json(`Menu item successfully deleted`);
+    } catch (err) {
+        console.error('Error deleting menu item:', err);
+        res.status(500).send('Error deleting menu item');
+    }
+});
+
 // Get Orders
 app.get("/orders", async (req, res) => {
     try {
@@ -392,7 +458,7 @@ app.post("/menu", async (req, res) => {
     try {
         const { menu_item, food_price, combo, menu_cat } = req.body;
         const q = `INSERT INTO menu (menu_item, menu_cat, combo, food_price)
-      VALUES ('${menu_item}', '${menu_cat}', ${combo}, ${food_price});`;
+      VALUES ('${menu_item}', '${menu_cat}', '${combo}', ${food_price});`;
         // console.log(q);
         const newItem = await pool.query(q);
         res.json(newItem.rows[0]);
